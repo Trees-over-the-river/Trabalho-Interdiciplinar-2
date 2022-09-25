286ec1bc6728e2b4126805df6933d73bd2b26518
mrnaru300 <bruno.santi.oli@gmail.com>
2022-09-25 20:57:25 -0300

    feat: created endpoints

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -2,7 +2,10 @@ package app;
 
 import com.google.gson.Gson;
 import dao.DAO;
+import dao.FilmeDAO;
 import dao.UsuarioDAO;
+import org.apache.http.MethodNotSupportedException;
+import service.FilmeService;
 import service.UsuarioService;
 import spark.Spark;
 
@@ -24,6 +27,7 @@ public class Aplicacao {
 
     private static final Gson gson = new Gson();
     private static final UsuarioService usuarioService = new UsuarioService(new UsuarioDAO(dao));
+    private static final FilmeService filmeService = new FilmeService(new FilmeDAO(dao));
 
     private static final int port = 6789;
 
@@ -40,6 +44,8 @@ public class Aplicacao {
 
         port(port);
 
+
+
         logger.info("Starting the server at http://localhost:" + port);
 
 
@@ -47,21 +53,43 @@ public class Aplicacao {
 
 
        path("/usuario/", () -> {
-          get(":id", usuarioService::getUsuarioByID);
+           path(":id", () -> {
+               get("/", usuarioService::getUsuarioByID);
+               put("nome", usuarioService::updateNome);
+               put("username", usuarioService::updateUsername);
+               put("email", usuarioService::updateEmail);
+               put("avatar", usuarioService::updateAvatar);
+               put("senha", usuarioService::updateSenha);
+               post("analitics/categoria", usuarioService::insertCategoriaPreferecia);
+               delete("/", usuarioService::deleteUsuario);
+           });
+
+          post("login", usuarioService::login);
+          post("logon", usuarioService::logon);
        });
 
+       path("/filme/:id", () -> {
+          get("/", filmeService::getFilmeByID);
+          get("avaliacoes", filmeService::getAvaliacoes);
+          post("avaliacao", filmeService::replaceAvaliacao);
+          get("categorias", filmeService::getCategoriasOfFilme);
+       });
 
+        exception(IllegalArgumentException.class, (e, request, response) ->
+                response.status(400));
 
-        exception(IllegalArgumentException.class, (e, request, response) -> {
-            response.status(400);
+        exception(MethodNotSupportedException.class, (e, request, response) -> {
+            response.status(501);
+            logger.warning(e.toString());
         });
 
-        exception(SQLException.class, (throwables, request, response) -> {
-            response.status(500);
-            logger.warning(throwables.toString());
+        exception(SQLException.class, (e, request, response) -> {
+            response.status(503);
+            logger.warning(e.toString());
         } );
 
         exception(RuntimeException.class, (e, request, response) -> {
+            response.status(500);
             logger.severe(e.toString() + "\n" + Arrays.toString(e.getStackTrace()));
         });
 
