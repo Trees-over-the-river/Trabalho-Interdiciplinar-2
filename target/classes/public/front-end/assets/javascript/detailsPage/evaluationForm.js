3eca2997e4b1e123f8b3e8b92eb43fcd376922e3
Fernando Campos Silva Dal' Maria <fernando.csm123@gmail.com>
2022-11-10 22:24:09 -0300

    a

--- target/classes/public/front-end/assets/javascript/detailsPage/evaluationForm.js
+++ target/classes/public/front-end/assets/javascript/detailsPage/evaluationForm.js
@@ -13,6 +13,10 @@ function testAv(res) {
         const urldetails = new URLSearchParams(window.location.search);
         let mID = urldetails.get('movie');
 
+        const API_MOVIE = `${baseUrl}/movie/${movieId}?api_key=${API_KEY}`;
+
+        fetchMovie(API_MOVIE, categIA, 'genres');
+
         const evaluationForm = document.querySelector('#evaluationForm');
         const confirmationDiv = document.querySelector('.confirmation-div');
 
@@ -49,4 +53,59 @@ function testAv(res) {
             confirmationDiv.style.display = 'block';
         }
     } 
+}
+
+function categIA(res) {
+    console.log(res);
+    let ids = [];
+    res.forEach(e => {
+        ids.push(e.id);
+    });
+
+    let preferGenres = JSON.parse(localStorage.getItem('genres'));
+    if(preferGenres == null) {
+        localStorage.setItem('preferido', JSON.stringify(countThis(ids)));
+        localStorage.setItem('genres', JSON.stringify(countingSort(ids)));
+    } else {
+        ids.forEach(e => preferGenres.push(e));
+        ids = countingSort(preferGenres);
+        localStorage.setItem('preferido', JSON.stringify(countThis(ids)));
+        localStorage.setItem('genres', JSON.stringify(ids));
+    }
+}
+
+function countThis(ids) {
+    let m = 1;
+    let c = 1;
+    let id = -1;
+    
+    for(let i = 0; i < ids.length - 1; i++) {
+        if(ids[i] === ids[i + 1]) c++;
+        else {
+            if(c >= m) {
+                m = c;
+                id = ids[i];
+            } 
+            c = 1;
+        }
+    }
+
+    return (ids.length == 1) ? ids[0] : id;
+}
+
+function countingSort(arr) {
+    let n = arr.length;
+    count = new Array(Math.max(...arr) + 1).fill(0);
+    out = new Array(n);
+
+    for(let i = 0; i < n; i++) count[arr[i]]++;
+
+    for(let i = 0; i < count.length - 1; i++) count[i + 1] += count[i];
+
+    for(let i = 0; i < n; i++) {
+        out[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    return out;
 }
